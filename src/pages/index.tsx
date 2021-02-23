10d17301de73b42e50ee5ca3b77a5b3c6906103b
guivicentep <guivicente.gvp@hotmail.com>
2021-02-23 17:03:31 -0300

    First commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import { ExperienceBar } from "../components/ExperienceBar";
+
+
+export default function Home() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+
+      <section>
+        <div>
+          
+        </div>
+        <div>
+
+        </div>
+      </section>
+    </div>
+  )
+}
