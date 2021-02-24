38d1c6aac6b1fbffbde19dc854b2049138e02650
guivicentep <guivicente.gvp@hotmail.com>
2021-02-24 12:47:44 -0300

    Adding code sponsors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,23 +9,23 @@ import styles from '../styles/pages/Home.module.css'
 
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Início | move.it</title>
-      </Head>
-      <ExperienceBar />
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Início | move.it</title>
+            </Head>
+            <ExperienceBar />
 
-      <section>
-        <div>
-          <Profile/>
-          <CompletedChallenges/>
-          <Countdown/>
-        </div>
-        <div>
+            <section>
+                <div>
+                    <Profile />
+                    <CompletedChallenges />
+                    <Countdown />
+                </div>
+                <div>
 
+                </div>
+            </section>
         </div>
-      </section>
-    </div>
-  )
+    )
 }
